68fdd3f9fac1a1f7eb682bdb271bdca6a7047934
Karol-Kaszuba <kar.kaszuba@gmail.com>
2025-03-20 21:25:25 +0100

    Zastąpienie bezpośrednich żądań HTTP obiektem WeatherClient w celu uproszczenia pobierania danych pogodowych

--- main.cpp
+++ main.cpp
@@ -1,37 +1,31 @@
-#include <boost/beast/core.hpp>
-#include <boost/beast/http.hpp>
-#include <boost/beast/version.hpp>
-#include <boost/asio/connect.hpp>
-#include <boost/asio/ip/tcp.hpp>
 #include <iostream>
+#include <weather_client.h>
+
 using namespace std;
-namespace beast = boost::beast;
-namespace http = beast::http;
-namespace net = boost::asio;
-using tcp = net::ip::tcp;
+
 int main()
 {
-    try
+    /**
+     *  inicjalizuje obiekt WeatherClient i ustawiam zmienne szerokości i długości geograficznej.
+     * @note
+     *       Szerokość geograficzna odpowiada równikowi (N/S), a długość geograficzna odpowiada południkowi zerowemu (E/W).
+     *       Dodatnie wartości szerokości geograficznej oznaczają półkulę północną (N), natomiast ujemne wartości oznaczają półkulę południową (S).
+     *       Dodatnie wartości długości geograficznej oznaczają półkulę wschodnią (E), natomiast ujemne wartości oznaczają półkulę zachodnią (W).
+     */
+
+    WeatherClient weatherClient_1;
+    double latitude = 52.400276;
+    double longitude = 16.955308;
+
+    string response_for_Client_1 = weatherClient_1.getWeatherData(latitude, longitude);
+
+    if (response_for_Client_1 == "")
     {
-        net::io_context ioc;
-        tcp::resolver resolver(ioc);
-        tcp::socket socket(ioc);
-        auto const results = resolver.resolve("api.open-meteo.com", "80");
-        net::connect(socket, results.begin(), results.end());
-http::request<http::string_body> req(http::verb::get,"/v1/forecast?latitude=52.400276&longitude=16.955308&current=temperature_2m,precipitation,weather_code,surface_pressure,wind_speed_10m", 11);
-req.set(http::field::host, "api.open-meteo.com");
-req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
-http::write(socket, req);
-beast::flat_buffer buffer;
-http::response<http::dynamic_body> res;
-http::read(socket, buffer, res);
-cout << res << endl;
-beast::error_code ec;
-socket.shutdown(tcp::socket::shutdown_both, ec);
+        cout << "Błąd pobierania danych." << endl;
     }
-    catch (std::exception const &e)
+    else
     {
-        cerr << "Wystąpił wyjątek: " << e.what() << endl;
+        cout << response_for_Client_1 << endl;
     }
     return 0;
 }
